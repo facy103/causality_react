91c3351336125bc64ae3308159c4042107ba4859
facy103 <orishlez@gmail.com>
2020-02-21 19:48:51 +0200

    Add files via upload

---
+++ build/precache-manifest.23bda64bfcc4775af301f9c0dbf6a7c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f41f091e5a4ae4e5c33966e47efaa23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d77f3364ae9da369817",
+    "url": "/static/css/main.c05339a3.chunk.css"
+  },
+  {
+    "revision": "3e44a104c737aee0a563",
+    "url": "/static/js/2.c8075570.chunk.js"
+  },
+  {
+    "revision": "f7c8b322a84e4a52583f130840fd8194",
+    "url": "/static/js/2.c8075570.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d77f3364ae9da369817",
+    "url": "/static/js/main.59c614fd.chunk.js"
+  },
+  {
+    "revision": "bc47861e4556222c49c1",
+    "url": "/static/js/runtime-main.1653fdf1.js"
+  },
+  {
+    "revision": "e0b443bcb7df8067cb8b6d96e6de0f6e",
+    "url": "/static/media/summary.e0b443bc.svg"
+  }
+]);
